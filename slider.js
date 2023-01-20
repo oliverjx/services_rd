29c75acb0efc719cdd89bb82c5a5baf0545a41f6
prettiestmf <2190038@ipisa.edu.do>
2023-01-19 23:19:31 -0800

    modifiqué parte del css del slider e hice que el slider se moviera tanto automatico como manual mediante javascript

--- slider.js
+++ slider.js
@@ -1,5 +1,5 @@
 const slider = document.querySelector("#slider");
-let sliderSection = document.querySelectorAll(",slider__section");
+let sliderSection = document.querySelectorAll(".slider__section");
 let sliderSectionLast = sliderSection[sliderSection.length -1];
 
 const btnLeft = document.querySelector("#btn-left");
@@ -8,28 +8,36 @@ const btnRight = document.querySelector("#btn-right");
 slider.insertAdjacentElement('afterbegin', sliderSectionLast);
 
 function Next() {
-    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
-    slider.style.marginLeft = "-200%";
-    slider.style.transition = "all 0.5s";
-    setTimeout(function() {
-        slider.style.transition = "none";
-        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
-        slider.style.marginLeft = "-100%";
-    }, 500);
+  let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+  slider.style.marginLeft = "-200%";
+  slider.style.transition = "all 0.5s";
+  setTimeout(function(){
+    slider.style.transition = "none";
+    slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+    slider.style.marginLeft = "-100%";
+  }, 500);
 }
 
 function Prev() {
-    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
-    slider.style.marginLeft = "-200%";
-    slider.style.transition = "all 0.5s";
-    setTimeout(function() {
-        slider.style.transition = "none";
-        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
-        slider.style.marginLeft = "-100%";
-    }, 500);
+  let sliderSection = document.querySelectorAll(".slider__section");
+  let sliderSectionLast = sliderSection[sliderSection.length -1];
+  slider.style.marginLeft = "0";
+  slider.style.transition = "all 0.5s";
+  setTimeout(function(){
+    slider.style.transition = "none";
+    slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+    slider.style.marginLeft = "-100%";
+  }, 500);
 }
 
-
-btnRight.addEvenetList('click', function(){
-    Next();
+btnRight.addEventListener('click', function(){
+  Next();
 });
+
+btnLeft.addEventListener('click', function(){
+  Prev();
+});
+
+setInterval(function(){
+  Next();
+}, 5000);
